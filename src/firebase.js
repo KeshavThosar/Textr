e88c212c53f2b6d42088927d921d15490db18bd5
Keshav Thosar <46895675+KeshavThosar@users.noreply.github.com>
2021-02-10 21:21:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDJHByHPneyZFQPTpwRiJYosJfdGuPFU0o",
-  authDomain: "react-chat-app-af783.firebaseapp.com",
-  projectId: "react-chat-app-af783",
-  storageBucket: "react-chat-app-af783.appspot.com",
-  messagingSenderId: "382388974456",
-  appId: "1:382388974456:web:a1601ce1d16f945c218e8d"
+//Firebase Config info
 };
 
   const app = firebase.initializeApp(firebaseConfig);
